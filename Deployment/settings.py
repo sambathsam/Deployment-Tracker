329701cdb88cf0cbfe3182d28fb98d7d4c5433d5
Sambath Kumar T <43872046+sambathsam@users.noreply.github.com>
2019-04-30 11:57:42 +0530

    Update settings.py

--- Deployment/settings.py
+++ Deployment/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'report',
         'USER': 'root',
-        'PASSWORD': 'admin@123',
+        'PASSWORD': '>DeE=Pe2B\33JqrA',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
